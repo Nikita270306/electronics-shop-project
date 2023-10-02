d7b85c61b61c15a6580f05bda9368d47e6f04c0a
nikita <n.shavkin@it-park.tech>
2023-10-02 19:11:50 +0300

    done

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+
+class Test():
+    def test_calculate_total_price(self):
+        item = Item("Бананы", 20, 1000)
+
+        assert item.calculate_total_price() == 20000
+
+    def test_apply_discount(self):
+        item = Item("Бананы", 20, 1000)
+
+        item.pay_rate = 0.5
+        item.apply_discount()
+        assert item.price == 10
+
